499781734f561903d40644492a72de2b6565cfa8
Niaz Morshed <niazmorshed2007@gmail.com>
2021-09-01 16:16:50 +0600

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Header from "./components/head/Header";
 import "./style/style.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">learn react</header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/head/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { FaShippingFast } from "react-icons/fa";
+
+function Header() {
+  return (
+    <div className="header">
+      <FaShippingFast />
+    </div>
+  );
+}
+
+export default Header;
