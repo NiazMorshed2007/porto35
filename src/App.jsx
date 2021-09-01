753f41ab0423304c964b6abab9345e785392797f
Niaz Morshed <niazmorshed2007@gmail.com>
2021-09-01 15:39:44 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style/style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">learn react</header>
+    </div>
+  );
+}
+
+export default App;
