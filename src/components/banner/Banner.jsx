c12c7e0df5c1511cdbb30b97c9bd9b32879a1f3e
Niaz Morshed <niazmorshed2007@gmail.com>
2021-09-02 18:26:56 +0600

    first commit

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { BsChevronRight, BsChevronLeft } from "react-icons/bs";
 
 function Banner() {
   return (
@@ -7,13 +8,21 @@ function Banner() {
         src="https://www.portotheme.com/wordpress/porto/shop35/wp-content/uploads/sites/178/2020/07/intro-1.jpg"
         alt=""
       />
-      <h4>Exclusive Product New Arrival</h4>
-      <h1>Organic Coffee</h1>
-      <h3>SPECIAL BLEND</h3>
-      <h5>Fresh!</h5>
-      <p>BREAKFAST PRODUCT ON SALE</p>
-      <h6>UP TO</h6>
-      <h2>50%</h2>
+      <div className="txt">
+        <h4>Exclusive Product New Arrival</h4>
+        <h1>Organic Coffee</h1>
+        <h3>SPECIAL BLEND</h3>
+        <h5>Fresh!</h5>
+        <p>BREAKFAST PRODUCT ON SALE</p>
+        <h3 className="up-to">UP TO</h3>
+        <h2>50%</h2>
+      </div>
+      <div className="btn-right">
+        <BsChevronRight />
+      </div>
+      <div className="btn-left">
+        <BsChevronLeft />
+      </div>
     </div>
   );
 }
