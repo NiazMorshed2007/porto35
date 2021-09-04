515a753a2bd5457ac6e7c6a10205d34565bf7114
Niaz Morshed <niazmorshed2007@gmail.com>
2021-09-04 11:50:50 +0600

    init

--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -2,13 +2,26 @@ import React from "react";
 import RecipesBox from "./RecipesBox";
 
 function Recipes() {
+  const arr = [
+    {
+      src: "https://www.portotheme.com/wordpress/porto/shop35/wp-content/uploads/sites/178/2020/07/blog-2.png",
+      head: "Pasta With Pesto",
+      class: "left",
+    },
+    {
+      src: "https://www.portotheme.com/wordpress/porto/shop35/wp-content/uploads/sites/178/2020/07/blog-1.png",
+      head: "Strawberry Waffles",
+      class: "right",
+    },
+  ];
   return (
     <div className="recipes">
       <h2>Recipes For This Week</h2>
       <p>All our new arrivals in a exclusive brand selection</p>
       <div className="box-wrapper">
-        <div className="left"></div>
-        <duv className="right"></duv>
+        {arr.map((e, i) => (
+          <RecipesBox class={e.class} img={e.src} key={i} head={e.head} />
+        ))}
       </div>
     </div>
   );
--- src/components/recipes/RecipesBox.jsx
+++ src/components/recipes/RecipesBox.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
 
 function RecipesBox(props) {
   return (
-    <>
-      <div className="img-wra">
+    <div className={props.class}>
+      <div className="img-wrap">
         <img src={props.img} alt="" />
+        <i>
+          <BsSearch />
+        </i>
       </div>
       <p>fresh vegetables</p>
       <h3>{props.head}</h3>
       <h4>A tasty way to incorporate more veggies into your diet!</h4>
-    </>
+    </div>
   );
 }
 
